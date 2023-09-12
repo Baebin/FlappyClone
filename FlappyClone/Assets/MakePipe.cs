2df285d6c907b96b1e7cf5819c865d7650535f23
Baebin <63220297+Baebin@users.noreply.github.com>
2023-09-13 00:02:40 +0900

    Pipe Prefab Random Generator

---
+++ FlappyClone/Assets/MakePipe.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            timer = 0;
+            
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(-2.4f, Random.Range(-1.0f, 5.5f), 0);
+        }
+    }
+}
